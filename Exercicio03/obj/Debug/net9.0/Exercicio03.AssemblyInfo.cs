3dc5f8ca42dd49c2daf9b4ee0f8f030bc217406f
Juules20 <jucustodio2005@gmail.com>
2025-03-24 10:16:01 -0300

    Tarefas_LPR

--- Exercicio03/obj/Debug/net9.0/Exercicio03.AssemblyInfo.cs
+++ Exercicio03/obj/Debug/net9.0/Exercicio03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6d1113351ba59e85c493e438d0148ed570c70e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
